02e745d998f25270db21eb66a754e7383e5ccf1a
ty2437309 <tyost2@student.rcc.edu>
2015-01-06 20:03:03 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

---
+++ Class_Lab/HelloWorld/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Taylor Yost
+ * Created on January 6, 2015, 6:05 PM
+ * Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hello World"<<endl;
+    //Exit stage right!
+    return 0;
+}
+
