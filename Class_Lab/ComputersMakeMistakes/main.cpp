3438cb375d30afaa34e742c1efd0d4df22cb516d
ty2437309 <tyost2@student.rcc.edu>
2015-01-06 21:20:46 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

---
+++ Class_Lab/ComputersMakeMistakes/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Taylor Yost
+ * Created on January 6, 2015, 6:05 PM
+ * Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare variables
+    float a,b,c;
+    //Prompt user for a and b
+    cout<<"Input 2 short variable values"<<endl;
+    cin>>a>>b;
+    //Calculate the sum
+    c=a+b;
+    //Output the results
+    cout<<"The sum c=a+b -> "<<c<<endl;
+    //Exit stage right!
+    return 0;
+}
+
