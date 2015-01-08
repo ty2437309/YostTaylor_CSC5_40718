3ec982c92bbfe951fa0c4153ee234334a5e0c952
ty2437309 <tyost2@student.rcc.edu>
2015-01-07 22:23:48 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

---
+++ NetBeansInstallTest/main.cpp
@@ -0,0 +1,13 @@
+/* 
+ * File:   main.cpp
+ * Author: Taylor Yost
+ * Created on January 7, 2015, 21:49
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout<<"Has everything worked properly?"<<endl;
+    return 0;
+}
