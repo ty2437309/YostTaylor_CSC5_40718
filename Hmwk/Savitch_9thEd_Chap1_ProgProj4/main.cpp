a2472572f910d416135ea8c9087251727c4a57ca
ty2437309 <tyost2@student.rcc.edu>
2015-01-08 10:05:49 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

---
+++ Hmwk/Savitch_9thEd_Chap1_ProgProj4/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Taylor Yost
+ * Created on January 8, 2015, 09:52
+ * Purpose: Chapter 1 Programming Projects Homework (Savitch 9th)
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+const float G = 32; //(ft/s)
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    int s, d;
+    //Prompt User
+    cout<<"This program calculates the distance an object \n";
+    cout<<"would fall in a vacuum given a specific time. \n";
+    cout<<"Please enter a time in seconds: "<<endl;
+    cin>> s;
+    //Calculate
+    d = (G * s * s)/2;
+    //Return Result
+    cout<<"In a vacuum, an object would travel "<<d<<" ft. \n";
+    cout<<"if it fell for "<<s<<" seconds."<<endl;
+    //Exit stage right!
+    return 0;
+}
