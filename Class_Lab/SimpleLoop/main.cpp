e1d38b07481de87e724071ff184652b297763d74
ty2437309 <tyost2@student.rcc.edu>
2015-01-15 16:51:21 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

---
+++ Class_Lab/SimpleLoop/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Taylor Yost
+ * Created on January 15, 2015, 16:37
+ * Purpose: Simple Loop
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    int count_down;
+    //Prompt User
+    cout<<"Enter number of times to test..."<<endl;
+    cin>> count_down;
+    //Execute Loop
+    while (count_down > 0){
+        cout<<"Test";
+        count_down = count_down - 1;
+    }
+    return 0;
+}
+
