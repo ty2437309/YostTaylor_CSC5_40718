ff0dd5479c690f70552c961e09d0a3423b804cd3
ty2437309 <tyost2@student.rcc.edu>
2015-01-28 21:13:52 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

--- Project1/Project_V1/main.cpp
+++ Project1/Project_V1/main.cpp
@@ -15,14 +15,12 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-turn_1(char);
 //Execution Begins Here!
-
 int main(int argc, char** argv) {
     //Declare Variables
     int t_1=1, t_2=2, t_3=3, t_4=4, t_5=5;
     int t_6=6, t_7=7, t_8=8, t_9=9, t_10=10;
-    char s_1, s_2, s_3, s_4;
+    char sol_1='R', sol_2='Y', sol_3='Y', sol_4='O';
     char a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_10;
     char b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_10;
     char c_1, c_2, c_3, c_4, c_5, c_6, c_7, c_8, c_9, c_10;
@@ -31,30 +29,7 @@ int main(int argc, char** argv) {
     char f_1, f_2, f_3, f_4, f_5, f_6, f_7, f_8, f_9, f_10;
     char e_11, e_12, e_13, e_14, e_15, e_16, e_17, e_18, e_19, e_20;
     char f_11, f_12, f_13, f_14, f_15, f_16, f_17, f_18, f_19, f_20;
-    int turn_count=1;
-    //Attempt Turn Change
-    if(turn_count==1){
-        //Turn 1 Stuff
-        turn_1();
-    }else if(turn_count==2){
-        //Turn 2 Stuff
-    }else if(turn_count==3){
-        //Turn 3 Stuff
-    }else if(turn_count==4){
-        //Turn 4 Stuff
-    }else if(turn_count==5){
-        //Turn 5 Stuff
-    }else if(turn_count==6){
-        //Turn 6 Stuff
-    }else if(turn_count==7){
-        //Turn 7 Stuff
-    }else if(turn_count==8){
-        //Turn 8 Stuff
-    }else if(turn_count==9){
-        //Turn 9 Stuff
-    }else{
-        //Turn 10 Stuff
-    }
+    int turn_count=1;   
     
     cout<<"        // Mastermind // "<<endl;
     cout<<"Turns ~~~~~~~~~~~~~~~~~~~"<<endl;
@@ -79,8 +54,64 @@ int main(int argc, char** argv) {
     cout<<"  "<<t_10<<"   | "<<a_10<<"| "<<b_10<<"| "<<c_10<<"| "<<d_10<<"|"<<e_19<<" | "<<f_19<<"|"<<endl;
     cout<<"      |__|__|__|__|"<<e_20<<" | "<<f_20<<"|"<<endl;
     cout<<"      |    Solution:    |"<<endl;
-    cout<<"      | "<<s_1<<"  "<<s_2<<"  "<<s_3<<"  "<<s_4<<"      |"<<endl;
+    cout<<"      | "<<sol_1<<"  "<<sol_2<<"  "<<sol_3<<"  "<<sol_4<<"      |"<<endl;
     cout<<"      ~~~~~~~~~~~~~~~~~~~"<<endl;
+    //Attempt Turn Change
+    if(turn_count==1){
+        //Turn 1 Stuff
+        cout<<"Turn 1 - Enter 4 Colors:"<<endl;
+        cin>>a_1>>b_1>>c_1>>d_1;
+        //Turn 1, Position 1 Determination (Black)
+        if (a_1==sol_1){
+            e_1='B';
+        }
+        else
+        {
+            e_1=' ';
+        }
+        //Turn 1, Position 2 Determination (Black)
+        if (b_1==sol_2){
+            f_1='B';
+        }
+        else
+        {
+            f_1=' ';
+        }
+        //Turn 1, Position 3 Determination (Black)
+        if (c_1==sol_3){
+            e_2='B';
+        }
+        else
+        {
+            e_2=' ';
+        }
+        //Turn 1, Position 4 Determination (Black)
+        if (d_1==sol_4){
+            f_2='B';
+        }
+        else
+        {
+            f_2=' ';
+        }
+    }else if(turn_count==2){
+        //Turn 2 Stuff
+    }else if(turn_count==3){
+        //Turn 3 Stuff
+    }else if(turn_count==4){
+        //Turn 4 Stuff
+    }else if(turn_count==5){
+        //Turn 5 Stuff
+    }else if(turn_count==6){
+        //Turn 6 Stuff
+    }else if(turn_count==7){
+        //Turn 7 Stuff
+    }else if(turn_count==8){
+        //Turn 8 Stuff
+    }else if(turn_count==9){
+        //Turn 9 Stuff
+    }else{
+        //Turn 10 Stuff
+    }
     //Exit Stage Right!!
     return 0;
 }
