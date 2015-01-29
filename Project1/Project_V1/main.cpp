7815611449b9b9be63cd395aadfb3a564ec12a52
ty2437309 <tyost2@student.rcc.edu>
2015-01-28 18:05:12 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

---
+++ Project1/Project_V1/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File: main.cpp
+ * Author: Taylor Yost
+ * Created on MMMM DD, YYYY, TT:TT
+ * Purpose: 
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    //Exit Stage Right!!
+    return 0;
+}
