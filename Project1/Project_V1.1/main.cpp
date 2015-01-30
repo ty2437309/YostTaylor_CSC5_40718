211f3992ae63414746b6cbb2f8930d76277393a4
ty2437309 <tyost2@student.rcc.edu>
2015-01-29 17:31:23 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

--- Project1/Project_V1.1/main.cpp
+++ Project1/Project_V1.1/main.cpp
@@ -17,55 +17,55 @@ using namespace std;
 
 //Function Prototypes
         //Turn 1 Feedback Logic Functions
-turn_1_pos_1(char, char, char, char, char, char, char);
-turn_1_pos_2(char, char, char, char, char, char, char);
-turn_1_pos_3(char, char, char, char, char, char, char);
-turn_1_pos_4(char, char, char, char, char, char, char);
+char turn_1_pos_1(char, char, char, char, char, char, char);
+char turn_1_pos_2(char, char, char, char, char, char, char);
+char turn_1_pos_3(char, char, char, char, char, char, char);
+char turn_1_pos_4(char, char, char, char, char, char, char);
         //Turn 2 Feedback Logic Functions
-turn_2_pos_1(char, char, char, char, char, char, char);
-turn_2_pos_2(char, char, char, char, char, char, char);
-turn_2_pos_3(char, char, char, char, char, char, char);
-turn_2_pos_4(char, char, char, char, char, char, char);
+char turn_2_pos_1(char, char, char, char, char, char, char);
+char turn_2_pos_2(char, char, char, char, char, char, char);
+char turn_2_pos_3(char, char, char, char, char, char, char);
+char turn_2_pos_4(char, char, char, char, char, char, char);
         //Turn 3 Feedback Logic Functions
-turn_3_pos_1(char, char, char, char, char, char, char);
-turn_3_pos_2(char, char, char, char, char, char, char);
-turn_3_pos_3(char, char, char, char, char, char, char);
-turn_3_pos_4(char, char, char, char, char, char, char);
+char turn_3_pos_1(char, char, char, char, char, char, char);
+char turn_3_pos_2(char, char, char, char, char, char, char);
+char turn_3_pos_3(char, char, char, char, char, char, char);
+char turn_3_pos_4(char, char, char, char, char, char, char);
         //Turn 4 Feedback Logic Functions
-turn_4_pos_1(char, char, char, char, char, char, char);
-turn_4_pos_2(char, char, char, char, char, char, char);
-turn_4_pos_3(char, char, char, char, char, char, char);
-turn_4_pos_4(char, char, char, char, char, char, char);
+char turn_4_pos_1(char, char, char, char, char, char, char);
+char turn_4_pos_2(char, char, char, char, char, char, char);
+char turn_4_pos_3(char, char, char, char, char, char, char);
+char turn_4_pos_4(char, char, char, char, char, char, char);
         //Turn 5 Feedback Logic Functions
-turn_5_pos_1(char, char, char, char, char, char, char);
-turn_5_pos_2(char, char, char, char, char, char, char);
-turn_5_pos_3(char, char, char, char, char, char, char);
-turn_5_pos_4(char, char, char, char, char, char, char);
+char turn_5_pos_1(char, char, char, char, char, char, char);
+char turn_5_pos_2(char, char, char, char, char, char, char);
+char turn_5_pos_3(char, char, char, char, char, char, char);
+char turn_5_pos_4(char, char, char, char, char, char, char);
         //Turn 6 Feedback Logic Functions
-turn_6_pos_1(char, char, char, char, char, char, char);
-turn_6_pos_2(char, char, char, char, char, char, char);
-turn_6_pos_3(char, char, char, char, char, char, char);
-turn_6_pos_4(char, char, char, char, char, char, char);
+char turn_6_pos_1(char, char, char, char, char, char, char);
+char turn_6_pos_2(char, char, char, char, char, char, char);
+char turn_6_pos_3(char, char, char, char, char, char, char);
+char turn_6_pos_4(char, char, char, char, char, char, char);
         //Turn 7 Feedback Logic Functions
-turn_7_pos_1(char, char, char, char, char, char, char);
-turn_7_pos_2(char, char, char, char, char, char, char);
-turn_7_pos_3(char, char, char, char, char, char, char);
-turn_7_pos_4(char, char, char, char, char, char, char);
+char turn_7_pos_1(char, char, char, char, char, char, char);
+char turn_7_pos_2(char, char, char, char, char, char, char);
+char turn_7_pos_3(char, char, char, char, char, char, char);
+char turn_7_pos_4(char, char, char, char, char, char, char);
         //Turn 8 Feedback Logic Functions
-turn_8_pos_1(char, char, char, char, char, char, char);
-turn_8_pos_2(char, char, char, char, char, char, char);
-turn_8_pos_3(char, char, char, char, char, char, char);
-turn_8_pos_4(char, char, char, char, char, char, char);
+char turn_8_pos_1(char, char, char, char, char, char, char);
+char turn_8_pos_2(char, char, char, char, char, char, char);
+char turn_8_pos_3(char, char, char, char, char, char, char);
+char turn_8_pos_4(char, char, char, char, char, char, char);
         //Turn 9 Feedback Logic Functions
-turn_9_pos_1(char, char, char, char, char, char, char);
-turn_9_pos_2(char, char, char, char, char, char, char);
-turn_9_pos_3(char, char, char, char, char, char, char);
-turn_9_pos_4(char, char, char, char, char, char, char);
+char turn_9_pos_1(char, char, char, char, char, char, char);
+char turn_9_pos_2(char, char, char, char, char, char, char);
+char turn_9_pos_3(char, char, char, char, char, char, char);
+char turn_9_pos_4(char, char, char, char, char, char, char);
         //Turn 10 Feedback Logic Functions
-turn_10_pos_1(char, char, char, char, char, char, char);
-turn_10_pos_2(char, char, char, char, char, char, char);
-turn_10_pos_3(char, char, char, char, char, char, char);
-turn_10_pos_4(char, char, char, char, char, char, char);
+char turn_10_pos_1(char, char, char, char, char, char, char);
+char turn_10_pos_2(char, char, char, char, char, char, char);
+char turn_10_pos_3(char, char, char, char, char, char, char);
+char turn_10_pos_4(char, char, char, char, char, char, char);
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variables
@@ -79,31 +79,35 @@ int main(int argc, char** argv) {
     char d_1, d_2, d_3, d_4, d_5, d_6, d_7, d_8, d_9, d_10;
     char e_1, e_2, e_3, e_4, e_5, e_6, e_7, e_8, e_9, e_10;
     char f_1, f_2, f_3, f_4, f_5, f_6, f_7, f_8, f_9, f_10;
+    char g_1, g_2, g_3, g_4, g_5, g_6, g_7, g_8, g_9, g_10;
+    char h_1, h_2, h_3, h_4, h_5, h_6, h_7, h_8, h_9, h_10;
     char e_11, e_12, e_13, e_14, e_15, e_16, e_17, e_18, e_19, e_20;
-    char f_11, f_12, f_13, f_14, f_15, f_16, f_17, f_18, f_19, f_20;   
+    char f_11, f_12, f_13, f_14, f_15, f_16, f_17, f_18, f_19, f_20;
+    char g_11, g_12, g_13, g_14, g_15, g_16, g_17, g_18, g_19, g_20;
+    char h_11, h_12, h_13, h_14, h_15, h_16, h_17, h_18, h_19, h_20;
     for (int turn_count=1; end_cond=true; turn_count++){
         cout<<"        // Mastermind // "<<endl;
         cout<<"Turns ~~~~~~~~~~~~~~~~~~~"<<endl;
-        cout<<"  "<<t_1<<"   | "<<a_1<<"| "<<b_1<<"| "<<c_1<<"| "<<d_1<<"|"<<e_1<<" | "<<f_1<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_2<<" | "<<f_2<<"|"<<endl;
-        cout<<"  "<<t_2<<"   | "<<a_2<<"| "<<b_2<<"| "<<c_2<<"| "<<d_2<<"|"<<e_3<<" | "<<f_3<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_4<<" | "<<f_4<<"|"<<endl;
-        cout<<"  "<<t_3<<"   | "<<a_3<<"| "<<b_3<<"| "<<c_3<<"| "<<d_3<<"|"<<e_5<<" | "<<f_5<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_6<<" | "<<f_6<<"|"<<endl;
-        cout<<"  "<<t_4<<"   | "<<a_4<<"| "<<b_4<<"| "<<c_4<<"| "<<d_4<<"|"<<e_7<<" | "<<f_7<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_8<<" | "<<f_8<<"|"<<endl;
-        cout<<"  "<<t_5<<"   | "<<a_5<<"| "<<b_5<<"| "<<c_5<<"| "<<d_5<<"|"<<e_9<<" | "<<f_9<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_10<<" | "<<f_10<<"|"<<endl;
-        cout<<"  "<<t_6<<"   | "<<a_6<<"| "<<b_6<<"| "<<c_6<<"| "<<d_6<<"|"<<e_11<<" | "<<f_11<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_12<<" | "<<f_12<<"|"<<endl;
-        cout<<"  "<<t_7<<"   | "<<a_7<<"| "<<b_7<<"| "<<c_7<<"| "<<d_7<<"|"<<e_13<<" | "<<f_13<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_14<<" | "<<f_14<<"|"<<endl;
-        cout<<"  "<<t_8<<"   | "<<a_8<<"| "<<b_8<<"| "<<c_8<<"| "<<d_8<<"|"<<e_15<<" | "<<f_15<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_16<<" | "<<f_16<<"|"<<endl;
-        cout<<"  "<<t_9<<"   | "<<a_9<<"| "<<b_9<<"| "<<c_9<<"| "<<d_9<<"|"<<e_17<<" | "<<f_17<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_18<<" | "<<f_18<<"|"<<endl;
-        cout<<"  "<<t_10<<"   | "<<a_10<<"| "<<b_10<<"| "<<c_10<<"| "<<d_10<<"|"<<e_19<<" | "<<f_19<<"|"<<endl;
-        cout<<"      |__|__|__|__|"<<e_20<<" | "<<f_20<<"|"<<endl;
+        cout<<"  "<<t_1<<"   | "<<a_1<<"| "<<b_1<<"| "<<c_1<<"| "<<d_1<<"|"<<e_1<<" | "<<f_1<<"|"<<g_1<<" | "<<h_1<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_2<<" | "<<f_2<<"|"<<g_2<<" | "<<h_2<<"|"<<endl;
+        cout<<"  "<<t_2<<"   | "<<a_2<<"| "<<b_2<<"| "<<c_2<<"| "<<d_2<<"|"<<e_3<<" | "<<f_3<<"|"<<g_3<<" | "<<h_3<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_4<<" | "<<f_4<<"|"<<g_4<<" | "<<h_4<<"|"<<endl;
+        cout<<"  "<<t_3<<"   | "<<a_3<<"| "<<b_3<<"| "<<c_3<<"| "<<d_3<<"|"<<e_5<<" | "<<f_5<<"|"<<g_5<<" | "<<h_5<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_6<<" | "<<f_6<<"|"<<g_6<<" | "<<h_6<<"|"<<endl;
+        cout<<"  "<<t_4<<"   | "<<a_4<<"| "<<b_4<<"| "<<c_4<<"| "<<d_4<<"|"<<e_7<<" | "<<f_7<<"|"<<g_7<<" | "<<h_7<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_8<<" | "<<f_8<<"|"<<g_8<<" | "<<h_8<<"|"<<endl;
+        cout<<"  "<<t_5<<"   | "<<a_5<<"| "<<b_5<<"| "<<c_5<<"| "<<d_5<<"|"<<e_9<<" | "<<f_9<<"|"<<g_9<<" | "<<h_9<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_10<<" | "<<f_10<<"|"<<g_10<<" | "<<h_10<<"|"<<endl;
+        cout<<"  "<<t_6<<"   | "<<a_6<<"| "<<b_6<<"| "<<c_6<<"| "<<d_6<<"|"<<e_11<<" | "<<f_11<<"|"<<g_11<<" | "<<h_11<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_12<<" | "<<f_12<<"|"<<g_12<<" | "<<h_12<<"|"<<endl;
+        cout<<"  "<<t_7<<"   | "<<a_7<<"| "<<b_7<<"| "<<c_7<<"| "<<d_7<<"|"<<e_13<<" | "<<f_13<<"|"<<g_13<<" | "<<h_13<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_14<<" | "<<f_14<<"|"<<g_14<<" | "<<h_14<<"|"<<endl;
+        cout<<"  "<<t_8<<"   | "<<a_8<<"| "<<b_8<<"| "<<c_8<<"| "<<d_8<<"|"<<e_15<<" | "<<f_15<<"|"<<g_15<<" | "<<h_15<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_16<<" | "<<f_16<<"|"<<g_16<<" | "<<h_16<<"|"<<endl;
+        cout<<"  "<<t_9<<"   | "<<a_9<<"| "<<b_9<<"| "<<c_9<<"| "<<d_9<<"|"<<e_17<<" | "<<f_17<<"|"<<g_17<<" | "<<h_17<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_18<<" | "<<f_18<<"|"<<g_18<<" | "<<h_18<<"|"<<endl;
+        cout<<"  "<<t_10<<"   | "<<a_10<<"| "<<b_10<<"| "<<c_10<<"| "<<d_10<<"|"<<e_19<<" | "<<f_19<<"|"<<g_19<<" | "<<h_19<<"|"<<endl;
+        cout<<"      |__|__|__|__|"<<e_20<<" | "<<f_20<<"|"<<g_20<<" | "<<h_20<<"|"<<endl;
         cout<<"      |    Solution:    |"<<endl;
         cout<<"      | "<<sol_1<<"  "<<sol_2<<"  "<<sol_3<<"  "<<sol_4<<"      |"<<endl;
         cout<<"      ~~~~~~~~~~~~~~~~~~~"<<endl;
@@ -112,14 +116,7 @@ int main(int argc, char** argv) {
             //Turn 1 Stuff
             cout<<"Turn 1 - Enter 4 Colors:"<<endl;
             cin>>a_1>>b_1>>c_1>>d_1;
-            //Turn 1, Position 1 Determination (Black)
-            if (a_1==sol_1){
-            e_1='B';
-            }
-            else
-            {
-                e_1=' ';
-            }
+            
             //Turn 1, Position 2 Determination (Black)
             if (b_1==sol_2){
                 f_1='B';
@@ -236,4 +233,22 @@ int main(int argc, char** argv) {
     
     //Exit Stage Right!!
     return 0;
+}
+
+char turn_1_pos_1(char a_1, char e_1, char g_1,char sol_1, char sol_2, char sol_3, char sol_4){
+    if (a_1==sol_1){
+        e_1='B';
+    }
+    else
+    {
+        e_1=' ';
+    }
+    if (a_1==sol_2||a_1==sol_3||a_1==sol_4){
+        g_1='W';
+    }
+    else
+    {
+        g_1=' ';
+    }
+    return(e_1, g_1);
 }
--- Project1/Project_V1.1/nbproject/Package-Debug.bash
+++ Project1/Project_V1.1/nbproject/Package-Debug.bash
@@ -6,16 +6,16 @@
 
 # Macros
 TOP=`pwd`
-CND_PLATFORM=Cygwin_4.x_1-Windows
+CND_PLATFORM=Cygwin_4.x-Windows
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project_v1
-OUTPUT_BASENAME=project_v1
-PACKAGE_TOP_DIR=projectv1/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project_v1.1
+OUTPUT_BASENAME=project_v1.1
+PACKAGE_TOP_DIR=projectv1.1/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/projectv1/bin"
+makeDirectory "${NBTMPDIR}/projectv1.1/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/projectv1.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/projectv1.1.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/projectv1.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/projectv1.1.tar *
 checkReturnCode
 
 # Cleanup
