629aae6e99ceac006f3701a73dc3ed2996380201
ty2437309 <tyost2@student.rcc.edu>
2015-02-10 10:20:26 -0800

    Signed-off-by: ty2437309 <tyost2@student.rcc.edu>

--- Project2/TestStuff/main.cpp
+++ Project2/TestStuff/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
     //Code-maker Variables
     char pass=0;
     unsigned int cdindx=0;
-    char sol[NSOL]={'G', 'P', 'O', 'O'};
+    char sol[NSOL]={'O', 'O', 'P', 'G'};
     char code[NCOLOR]={'R', 'O', 'Y', 'G', 'B', 'P'};
     
     //Computer Makes Code
@@ -42,7 +42,7 @@ int main(int argc, char** argv) {
     cout<<"Solution: "<<sol[0]<<sol[1]<<sol[2]<<sol[3]<<endl;
     //Output for loop test
     int index=0, black=0, white=0, flag=0;
-    char turn1[NSOL]={'O', 'O', 'O', 'Y'};
+    char turn1[NSOL]={'Y', 'O', 'O', 'O'};
     //Check For Black
     for(int index=0;index<=3;index++){
         if(turn1[index]==sol[index]){
@@ -50,8 +50,9 @@ int main(int argc, char** argv) {
         }
     }
     //Check For White
+    index=0;
     for(int index=0;index<=3;index++){
-        if(index=0){
+        if(index==0){
             if((turn1[0]==sol[1])&&(turn1[0]!=sol[0])&&(turn1[1]!=sol[1])){
                 white++;
             }
@@ -60,9 +61,10 @@ int main(int argc, char** argv) {
             }
             if((turn1[0]==sol[3])&&(turn1[0]!=sol[0])&&(turn1[3]!=sol[3])){
                 white++;
-            }
-        }
-        if(index=1){
+            } 
+            cout<<"indx 0 white"<<white<<endl;
+        }        
+        if(index==1){
             if((turn1[1]==sol[0])&&(turn1[1]!=sol[1])&&(turn1[0]!=sol[0])){
                 white++;
             }
@@ -72,8 +74,9 @@ int main(int argc, char** argv) {
             if((turn1[1]==sol[3])&&(turn1[1]!=sol[1])&&(turn1[3]!=sol[3])){
                 white++;
             }
+            cout<<"indx 1 white"<<white<<endl;
         }
-        if(index=2){
+        if(index==2){
             if((turn1[2]==sol[0])&&(turn1[2]!=sol[2])&&(turn1[0]!=sol[0])){
                 white++;
             }
@@ -83,8 +86,9 @@ int main(int argc, char** argv) {
             if((turn1[2]==sol[3])&&(turn1[2]!=sol[2])&&(turn1[3]!=sol[3])){
                 white++;
             }
+            cout<<"indx 2 white"<<white<<endl;
         }
-        if(index=3){
+        if(index==3){
             if((turn1[3]==sol[0])&&(turn1[3]!=sol[3])&&(turn1[0]!=sol[0])){
                 white++;
             }
@@ -94,12 +98,13 @@ int main(int argc, char** argv) {
             if((turn1[3]==sol[2])&&(turn1[3]!=sol[3])&&(turn1[2]!=sol[2])){
                 white++;
             }
+            cout<<"indx 3 white"<<white<<endl;
         }
     }
     
     //Eliminate Duplicates
 
-    cout<<"Black: "<<black<<" White: "<<white<<endl;
+    cout<<"Total Black: "<<black<<" Total White: "<<white<<endl;
     return 0;
 }
 
